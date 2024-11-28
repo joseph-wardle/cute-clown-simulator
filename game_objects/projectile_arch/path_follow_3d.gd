85f573955eafb7ac7e75310979eeca7c06fab862
Pactace <iantheartist01@gmail.com>
2024-11-27 20:01:36 -0700

    The clown can now juggle 1 ball

--- game_objects/projectile_arch/path_follow_3d.gd
+++ game_objects/projectile_arch/path_follow_3d.gd
@@ -1,10 +1,9 @@
 @tool
 extends PathFollow3D
 
-@export var process: bool = false
-@export var speed: int = 50
+@export var process: bool = true
+@export var speed: int = 5
 
 func _process(delta):
 	if process:
 		progress += delta * speed
-		print(progress)
