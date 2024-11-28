351f9f1206045fb9dc7975da1320d335da6f92ac
joseph-wardle <joseph.m.wardle@gmail.com>
2024-11-27 20:31:33 -0700

    collapsed clown to single dimention

--- game_objects/ball/ball.gd
+++ game_objects/ball/ball.gd
@@ -1,12 +1,12 @@
 extends CharacterBody3D
 
 @export var max_lean_angle := 30.0
-@export var fall_speed := 20.0
-@export var lean_speed := 100.0
+@export var fall_speed := 50.0
+@export var lean_speed := 150.0
 @export var entropy_strength := 5.0
 @export var speed_multiplier := 0.2
 
-var lean_angle := Vector2.ZERO
+var lean_angle := 0.0
 
 func _physics_process(delta: float) -> void:
 	handle_input(delta)
@@ -17,36 +17,33 @@ func _physics_process(delta: float) -> void:
 func handle_input(delta: float) -> void:
 	var input_vector = Vector2.ZERO
 	
-	input_vector.x = Input.get_action_strength("tilt_right") - Input.get_action_strength("tilt_left")
-	input_vector.y = Input.get_action_strength("tilt_backward") - Input.get_action_strength("tilt_forward")
+	input_vector = Input.get_action_strength("tilt_left") - Input.get_action_strength("tilt_right")
 	
 	lean_angle += input_vector * lean_speed * delta
 	
-	if lean_angle.length() > 0:
-		lean_angle += lean_angle.normalized() * fall_speed * delta
+	if abs(lean_angle) > 0:
+		lean_angle += sign(lean_angle) * fall_speed * delta
 	
-	lean_angle.x += randf_range(-entropy_strength, entropy_strength) * delta
-	lean_angle.y += randf_range(-entropy_strength, entropy_strength) * delta
-	
+	lean_angle += randf_range(-entropy_strength, entropy_strength) * delta
+
 	
 func update_movement(delta: float) -> void:
 	var direction := Vector3(
-		sin(deg_to_rad(lean_angle.x)),
+		sin(deg_to_rad(lean_angle)),
 		0,
-		sin(deg_to_rad(lean_angle.y))
+		0,
 	)
 	
-	var speed = lean_angle.length() * speed_multiplier
+	var speed = abs(lean_angle) * speed_multiplier
 	
 	velocity = direction.normalized() * speed
 	
 	move_and_slide()
 	
-	if lean_angle.length() >= max_lean_angle:
+	if abs(lean_angle) >= max_lean_angle:
 		print("The clown has fallen!")
 	
 	
 	
 func update_visuals(delta: float) -> void:
-	$Clown.rotation_degrees.y = -lean_angle.y
-	$Clown.rotation_degrees.z = lean_angle.x
+	$Clown.rotation_degrees.z = lean_angle
