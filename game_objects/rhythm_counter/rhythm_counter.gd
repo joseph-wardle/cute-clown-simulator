613ed23a936fad6d429735ffade5536803062307
joseph-wardle <joseph.m.wardle@gmail.com>
2024-11-29 12:54:57 -0700

    added rhythm counter
    includes perfect, good, ok and miss input reading

---
+++ game_objects/rhythm_counter/rhythm_counter.gd
@@ -0,0 +1,68 @@
+extends Timer
+
+enum InputType {
+	PERFECT,
+	GOOD,
+	OK,
+	MISS
+}
+	
+signal beat
+signal input_result(input_type: InputType)
+
+@export var perfect_window := 100.0 # 100 ms (+/- 50 ms)
+@export var good_window := 200.0    # 200 ms (+/- 100 ms)
+@export var ok_window := 300.0      # 300 ms (+/- 150 ms)
+
+var bpm: float:
+	get:
+		return 60.0 / wait_time if wait_time > 0 else 0.0
+	set(value):
+		wait_time = 60.0 / value if value > 0 else 0.0
+
+var last_beat_time: float = 0.0
+var perfect_half_window: float
+var good_half_window: float
+var ok_half_window: float
+
+func _ready() -> void:
+	_update_half_windows()
+	
+	
+func _update_half_windows() -> void:
+	perfect_half_window = perfect_window / 2
+	good_half_window = good_window / 2
+	ok_half_window = ok_window / 2
+
+
+func _input(event):
+	if event.is_action_pressed("beat"):
+		var input_time := Time.get_ticks_msec()
+		var delta := _calculate_delta(input_time)
+		var result := _get_input_result(delta)
+		emit_signal("input_result", result)
+		print(result)
+
+
+func _calculate_delta(input_time: float) -> float:
+	var beat_interval := wait_time * 1000.0 # convert to ms
+	var ms_since_last_beat := input_time - last_beat_time
+	var delta := fposmod(ms_since_last_beat, beat_interval)
+	if delta > beat_interval / 2:
+		delta = beat_interval - delta
+	return delta
+	
+	
+func _get_input_result(delta: float) -> InputType:
+	if delta <= perfect_half_window:
+		return InputType.PERFECT
+	elif delta <= good_half_window:
+		return InputType.GOOD
+	elif delta <= ok_half_window:
+		return InputType.OK
+	else:
+		return InputType.MISS
+
+func _on_timeout() -> void:
+	emit_signal("beat")
+	last_beat_time = Time.get_ticks_msec()
