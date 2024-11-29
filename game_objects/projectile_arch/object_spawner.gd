9572bb09aa9728ec8fd28b51aeeddd8f27c4a4ef
Pactace <iantheartist01@gmail.com>
2024-11-29 15:34:28 -0700

    BALLS

---
+++ game_objects/projectile_arch/object_spawner.gd
@@ -0,0 +1,26 @@
+extends Path3D
+
+var timer = 0
+var spawnTime = 3
+
+func add_object():
+	#Ok so the first thing we are going to do is add a new pathFollowInstance
+	var follower = PathFollow3D.new()
+	
+	#After that we are going to add the mesh instance. set it.
+	var mesh = MeshInstance3D.new()
+	mesh.scale.x = -0.236
+	mesh.scale.y = -0.239
+	mesh.scale.z = -0.232
+	mesh.set_mesh(SphereMesh.new())
+	var script = load("res://game_objects/projectile_arch/path_follow_3d.gd")
+	follower.add_child(mesh)
+	follower.script = script
+	add_child(follower)
+
+func _process(delta):
+	timer = timer + delta
+	print(timer)
+	if(timer > spawnTime):
+		timer = 0
+		add_object()
