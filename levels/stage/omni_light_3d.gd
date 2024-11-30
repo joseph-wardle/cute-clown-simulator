ff5f62204464cd0a5cfe58d06cee2d7b4358c2f1
joseph-wardle <joseph.m.wardle@gmail.com>
2024-11-29 18:25:23 -0700

    fixed light transformation

--- levels/stage/omni_light_3d.gd
+++
@@ -1,7 +0,0 @@
-extends OmniLight3D
-
-func _process(delta):
-	var target_node = get_node("../Ball")
-	if target_node:
-		#global_transform.origin = target_node.global_transform.origin
-		global_transform.origin.x = target_node.global_transform.origin.x
--- levels/stage/spot_light_3d.gd
+++
@@ -1,1 +0,0 @@
-extends SpotLight3D
