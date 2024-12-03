f07374d4e72434a8f27bccd340ba20790133a135
Pactace <iantheartist01@gmail.com>
2024-12-02 22:00:37 -0700

    Made the B A L L go at the timing of the beat

--- game_objects/juggling_arch/path_follow_3d.gd
+++ game_objects/juggling_arch/path_follow_3d.gd
@@ -2,8 +2,16 @@
 extends PathFollow3D
 
 @export var process: bool = true
-@export var speed: int = 5
+@onready var timer = get_node("../RhythmCounter")
 
 func _process(delta):
 	if process:
-		progress += delta * speed
+		# Calculate the beat duration from the timer's BP0
+		var beat_duration = 60 / timer.bpm
+		# Increment progress to complete one loop in one beat
+		progress_ratio += delta/beat_duration
+		# Wrap progress to ensure it stays between 0 and 1 
+
+
+func _on_rhythm_counter_timeout() -> void:
+	progress_ratio = 1 
