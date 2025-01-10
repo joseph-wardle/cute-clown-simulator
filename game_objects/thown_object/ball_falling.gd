11184df5842ff617877491b8164586a7f8b9528d
joseph-wardle <joseph.m.wardle@gmail.com>
2025-01-10 00:22:10 -0800

    Removed unused code

--- game_objects/thown_object/ball_falling.gd
+++
@@ -1,5 +0,0 @@
-extends Area3D
-
-func _on_area_entered(area: Area3D) -> void:
-	visible = false
-	position.y = -25
--- game_objects/thown_object/fall_down.gd
+++
@@ -1,6 +0,0 @@
-extends Path3D
-
-var rng = RandomNumberGenerator.new()
-
-func _ready():
-	position.x = rng.randf_range(-15, 15)
